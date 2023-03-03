f4a62f243b9ce70c3b9613639acd6c28db34a1c2
Yuta Okuma <y.dream.nest@icloud.com>
2023-03-03 09:08:17 +0900

    feat: :sparkles: 特性に応じてCSSを分類する

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   rules: {
     'no-descending-specificity': null,
+    'selector-class-pattern': '^(([a-z][a-zA-Z0-9_]+)|([a-z][a-z0-9]*)(-[a-zA-Z0-9_]+)*)$',
   },
 };
